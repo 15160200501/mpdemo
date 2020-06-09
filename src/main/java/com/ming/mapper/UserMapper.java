9564553f3687fc85444431a9bf471291cfe629ed
Ming-1c <a1160257452@gmail.com>
2020-06-09 14:38:53 +0800

    接口

---
+++ src/main/java/com/ming/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.ming.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ming.entity.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+
+
+
+}
